6e901c814bec811b13980495330dc4c84819d116
Oleg Trokay <trokay70@gmail.com>
2022-05-14 02:16:06 +0300

    fix3

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -3,13 +3,14 @@ package com.example
 import com.sun.net.httpserver.HttpServer
 import com.example.services.MainResource
 import com.example.services.ParameterFilter
+import java.net.InetAddress
 import java.net.InetSocketAddress
 import java.util.concurrent.Executors
 
 
 fun main(args: Array<String>) {
     println("Server is started!")
-    val server = HttpServer.create(InetSocketAddress("127.0.0.1", 8081), 0)
+    val server = HttpServer.create(InetSocketAddress(InetAddress.getByAddress("https://clearserver-production.up.railway.app/".toByteArray()), 8080), 0)
     val serverContext = server.createContext(
         "/",
         MainResource(UsersDataSource())
