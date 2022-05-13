87ec4cca1dde659665e926dedc3af314b8f6f084
Oleg Trokay <trokay70@gmail.com>
2022-05-14 02:22:40 +0300

    fix3

--- src/main/kotlin/com/example/Main.kt
+++ src/main/kotlin/com/example/Main.kt
@@ -11,7 +11,7 @@ import java.util.concurrent.Executors
 fun main(args: Array<String>) {
     println("Server is started!")
     InetAddress.getByName("")
-    val server = HttpServer.create(InetSocketAddress(InetAddress.getByName("https://clearserver-production.up.railway.app/"), 8080), 0)
+    val server = HttpServer.create(InetSocketAddress(InetAddress.getByName("clearserver-production.up.railway.app"), 8080), 0)
     val serverContext = server.createContext(
         "/",
         MainResource(UsersDataSource())
